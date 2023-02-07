45a28485d10d1457c8e11ab6a72ef39879e41652
Ewan Yuanxi Li <79686539+yuanxili1@users.noreply.github.com>
2023-02-07 01:06:26 -0600

    Initialize contract and deployment scrips

---
+++ contracts/contract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+//
+// Solidity Contract to be deployed on the local blockchain
+//
+
+pragma solidity ^0.8.17;
+
+// TODO: Add the contract code here
---
+++ scripts/deploy.js
@@ -0,0 +1,11 @@
+// This script deploys the integer mutation contract on the local chain
+//
+
+// Import hardhat
+const hre = require("hardhat");
+
+// TODO: Add a main function
+
+
+// TODO: use async/await everywhere
+// and properly handle errors.
