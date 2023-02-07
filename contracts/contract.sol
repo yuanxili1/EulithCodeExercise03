d2a67140434aad40632e1d56c11e52938f804e77
Ewan Yuanxi Li <79686539+yuanxili1@users.noreply.github.com>
2023-02-07 13:05:42 -0600

    Update contract scrips

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -5,4 +5,24 @@
 
 pragma solidity ^0.8.17;
 
-// TODO: Add the contract code here
+// This is a simple contract that will allow you to 
+// increment, decrement, set, and get an integer
+contract mutateInterger{
+    uint256 public _integer = 0;
+    
+    function increment() public {
+        _integer++;
+    }
+    
+    function decrement() public {
+        _integer--;
+    }
+
+    function setInteger(uint256 _newInteger) public {
+        _integer = _newInteger;
+    }
+    
+    function getInteger() public view returns (uint256){
+        return _integer;
+    }
+}
